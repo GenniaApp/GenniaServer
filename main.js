3f67fe3570283ddbd72c8bce99d80ca2caa6a7e4
Reqwey <50829219+Reqwey@users.noreply.github.com>
2022-11-07 19:18:53 +0800

    Migrate core from GenniaApp 1.0.0

---
+++ main.js
@@ -0,0 +1,375 @@
+/*
+ * 
+ * Gennia server side
+ * Copyright (c) 2022 Reqwey Lin (https://github.com/Reqwey)
+ * 
+ */
+
+// Modules to control application life and create native browser window
+const path = require('path')
+const { Server } = require("socket.io")
+const crypto = require('crypto');
+const GameMap = require("./src/server/map")
+const Point = require("./src/server/point")
+const Player = require("./src/server/player")
+
+const speedArr = [0.25, 0.5, 0.75, 1, 2, 3, 4]
+const forceStartOK = [1, 2, 2, 3, 3, 4, 5, 5, 6]
+//                    0  1  2  3  4  5  6  7  8
+
+global.username = ''
+global.port = 9016
+global.serverRunning = false
+global.gameStarted = false
+global.map = undefined
+global.gameConfig = {
+  maxPlayers: 8,
+  gameSpeed: 3,
+  mapWidth: 0.75,
+  mapHeight: 0.75,
+  mountain: 0.5,
+  city: 0.5,
+  swamp: 0
+}
+global.players = new Array()
+global.generals = new Array()
+global.forceStartNum = 0
+
+async function userSet(username) {
+  // console.log('join', this)
+  // // this === socket
+  // console.info(`Add player | ${this.id}| ${username}`)
+  // global.game.tryToAddPlayer.push([this, username])
+}
+
+async function handleInput(command) {
+  // console.info(`Get command | ${this.id}`)
+  // global.game.handleInput(this, command)
+}
+
+async function handleDisconnectInGame(player, io) {
+  io.local.emit('room_message', player.trans(), 'quit.')
+  global.players = global.players.filter(p => p != player)
+}
+
+async function handleDisconnectInRoom(player, io) {
+  io.local.emit('room_message', player.trans(), 'quit.')
+  let newPlayers = []
+  for (let i = 0, c = 0; i < global.players.length; ++i) {
+    if (global.players[i].id !== player.id) {
+      global.players[i].color = c++
+      newPlayers.push(global.players[i])
+    } else {
+      if (global.players[i].forceStart) {
+        --global.forceStartNum
+        io.local.emit('force_start_changed', global.forceStartNum)
+      }
+    }
+  }
+  global.players = newPlayers
+  if (global.players.length > 0) global.players[0].setRoomHost(true)
+  io.local.emit('players_changed', global.players.map(player => player.trans()))
+}
+
+async function getPlayerIndex(playerId) {
+  for (let i = 0, c = 0; i < global.players.length; ++i) {
+    if (global.players[i].id === playerId) {
+      return i
+    }
+  }
+  return -1
+}
+
+async function getPlayerIndexBySocket(socketId) {
+  for (let i = 0, c = 0; i < global.players.length; ++i) {
+    if (global.players[i].socket_id === socketId) {
+      return i
+    }
+  }
+  return -1
+}
+
+async function handleGame(io) {
+  if (global.gameStarted === false) {
+    console.info(`Start game`)
+    for (let [id, socket] of io.sockets.sockets) {
+      let playerIndex = await getPlayerIndexBySocket(id)
+      socket.emit('game_started', global.players[playerIndex].color)
+    }
+    global.gameStarted = true
+
+    global.map = new GameMap(
+      global.gameConfig.mapWidth,
+      global.gameConfig.mapHeight,
+      global.gameConfig.mountain,
+      global.gameConfig.city,
+      global.gameConfig.swamp,
+      global.players
+    )
+    global.players = await global.map.generate()
+    global.mapGenerated = true
+
+    io.local.emit('init_game_map', global.map.width, global.map.height)
+
+    for (let [id, socket] of io.sockets.sockets) {
+      socket.on('attack', async (from, to, isHalf) => {
+        let playerIndex = await getPlayerIndexBySocket(id)
+        let player = global.players[playerIndex]
+        if (player.operatedTurn < global.map.turn && global.map.commandable(player, from, to)) {
+          if (isHalf) {
+            global.map.moveHalfMovableUnit(player, from, to)
+          } else {
+            global.map.moveAllMovableUnit(player, from, to)
+          }
+
+          global.players[playerIndex].operatedTurn = global.map.turn
+          socket.emit('attack_success', from, to)
+        } else {
+          socket.emit('attack_failure', from, to)
+        }
+      })
+    }
+
+    let updTime = 500 / speedArr[global.gameConfig.gameSpeed]
+    let gameLoop = setInterval(async () => {
+      try {
+
+        global.players.forEach(async (player) => {
+          let block = global.map.getBlock(player.king)
+
+          let blockPlayerIndex = await getPlayerIndex(block.player.id)
+          if (blockPlayerIndex !== -1) {
+            if (block.player !== player && player.isDead === false) {
+              console.log(block.player.username, 'captured', player.username)
+              io.local.emit('captured', block.player.trans(), player.trans())
+              io.sockets.sockets.get(player.socket_id).emit('game_over', block.player.trans())
+              player.isDead = true
+
+              player.land.forEach(block => {
+                global.map.transferBlock(block, global.players[blockPlayerIndex])
+                global.players[blockPlayerIndex].winLand(block)
+              })
+              player.land.length = 0
+            }
+          }
+        })
+        let alivePlayer = null, countAlive = 0
+        for (let a of global.players)
+          if (!a.isDead) alivePlayer = a, ++countAlive
+        if (countAlive === 1) {
+          io.local.emit('game_ended', alivePlayer.id)
+          global.gameStarted = false
+          clearInterval(gameLoop)
+        }
+
+        let leaderBoard = global.players.map(player => {
+          let data = global.map.getTotal(player)
+          return {
+            color: player.color,
+            username: player.username,
+            army: data.army,
+            land: data.land
+          }
+        }).sort((a, b) => { return b.army - a.army || b.land - a.land })
+
+
+        io.local.emit('leaderboard', leaderBoard)
+
+        for (let [id, socket] of io.sockets.sockets) {
+          let playerIndex = await getPlayerIndexBySocket(id)
+          if (playerIndex !== -1) {
+            let view = await global.map.getViewPlayer(global.players[playerIndex])
+            view = JSON.stringify(view)
+            socket.emit('game_update', view, global.map.width, global.map.height, global.turn)
+          }
+        }
+        global.map.updateTurn()
+        global.map.updateUnit()
+      } catch (e) { console.log(e) }
+    }, updTime)
+  }
+}
+
+io = new Server(global.port)
+global.serverRunning = true
+console.log('Server established')
+// Listen for socket.io connections
+
+// io.on('connect', async (socket) => {
+//   console.log(io.sockets.sockets)
+// })
+io.on('connection', async (socket) => {
+  if (global.players.length >= global.gameConfig.maxPlayers) socket.emit('reject_join', 'The room is full.')
+  else {
+    let player;
+
+    socket.on('reconnect', async (playerId) => {
+      try {
+        if (global.gameStarted) { // Allow to reconnect
+          let playerIndex = await getPlayerIndex(playerId)
+          player = global.players[playerIndex]
+          global.players[playerIndex].socket_id = socket.id
+          io.local.emit('room_message', player.trans(), 're-joined the lobby.')
+        }
+      } catch (e) {
+        socket.emit('error', 'An unknown error occurred: ' + e.message, e.stack)
+      }
+    })
+
+    socket.on('set_username', async (username) => {
+      if (global.gameStarted) {
+        socket.emit('reject_join', 'Game is already started')
+        return;
+      }
+      // This socket will be first called when the player connects the server
+      let playerId = crypto.randomBytes(Math.ceil(10 / 2)).toString('hex').slice(0, 10)
+      console.log("Player:", username, "playerId:", playerId)
+      socket.emit('set_player_id', playerId)
+
+      player = new Player(playerId, socket.id, username, global.players.length)
+
+      global.players.push(player)
+      let playerIndex = global.players.length - 1
+
+      io.local.emit('room_message', player.trans(), 'joined the lobby.')
+      io.local.emit('players_changed', global.players.map(player => player.trans()))
+
+      if (global.players.length === 1) {
+        console.log(global.players[playerIndex])
+        global.players[playerIndex].setRoomHost(true)
+      }
+      global.players[playerIndex].username = username
+      io.local.emit('players_changed', global.players.map(player => player.trans()))
+
+      // Only emit to this player so it will get the latest status
+      socket.emit('force_start_changed', global.forceStartNum)
+
+      if (global.players.length >= global.gameConfig.maxPlayers) {
+        await handleGame(io)
+      }
+    })
+
+    socket.on('get_game_settings', async () => {
+      socket.emit('push_game_settings', global.gameConfig)
+    })
+
+    socket.on('change_host', async (userId) => {
+      if (player.isRoomHost) {
+        let currentHost = await getPlayerIndex(player.id)
+        let newHost = await getPlayerIndex(userId)
+        if (newHost !== -1) {
+          global.players[currentHost].setRoomHost(false)
+          global.players[newHost].setRoomHost(true)
+          io.local.emit('players_changed', global.players.map(player => player.trans()))
+        }
+      }
+    })
+
+    socket.on('change_game_speed', async (value) => {
+      if (player.isRoomHost) {
+        console.log('Changing game speed to ' + speedArr[value] + 'x')
+        global.gameConfig.gameSpeed = value
+        io.local.emit('game_config_changed', global.gameConfig)
+        io.local.emit('room_message', player.trans(), `changed the game speed to ${speedArr[value]}x.`)
+      } else {
+        socket.emit('error', 'Changement was failed', 'You are not the game host.')
+      }
+    })
+
+    socket.on('change_map_width', async (value) => {
+      if (player.isRoomHost) {
+        console.log('Changing map width to' + value)
+        global.gameConfig.mapWidth = value
+        io.local.emit('game_config_changed', global.gameConfig)
+        io.local.emit('room_message', player.trans(), `changed the map width to ${value}.`)
+      } else {
+        socket.emit('error', 'Changement was failed', 'You are not the game host.')
+      }
+    })
+
+    socket.on('change_map_height', async (value) => {
+      if (player.isRoomHost) {
+        console.log('Changing map height to' + value)
+        global.gameConfig.mapHeight = value
+        io.local.emit('game_config_changed', global.gameConfig)
+        io.local.emit('room_message', player.trans(), `changed the map height to ${value}.`)
+      } else {
+        socket.emit('error', 'Changement was failed', 'You are not the game host.')
+      }
+    })
+
+    socket.on('change_mountain', async (value) => {
+      if (player.isRoomHost) {
+        console.log('Changing mountain to' + value)
+        global.gameConfig.mountain = value
+        io.local.emit('game_config_changed', global.gameConfig)
+        io.local.emit('room_message', player.trans(), `changed the mountain to ${value}.`)
+      } else {
+        socket.emit('error', 'Changement was failed', 'You are not the game host.')
+      }
+    })
+
+    socket.on('change_city', async (value) => {
+      if (player.isRoomHost) {
+        console.log('Changing city to' + value)
+        global.gameConfig.city = value
+        io.local.emit('game_config_changed', global.gameConfig)
+        io.local.emit('room_message', player.trans(), `changed the city to ${value}.`)
+      } else {
+        socket.emit('error', 'Changement was failed', 'You are not the game host.')
+      }
+    })
+
+    socket.on('change_swamp', async (value) => {
+      if (player.isRoomHost) {
+        console.log('Changing swamp to' + value)
+        global.gameConfig.swamp = value
+        io.local.emit('game_config_changed', global.gameConfig)
+        io.local.emit('room_message', player.trans(), `changed the swamp to ${value}.`)
+      } else {
+        socket.emit('error', 'Changement was failed', 'You are not the game host.')
+      }
+    })
+
+    socket.on('change_max_player_num', async (value) => {
+      if (player.isRoomHost) {
+        console.log('Changing max players to' + value)
+        global.gameConfig.maxPlayers = value
+        io.local.emit('game_config_changed', global.gameConfig)
+        io.local.emit('room_message', player.trans(), `changed the max player num to ${value}.`)
+      } else {
+        socket.emit('error', 'Changement was failed', 'You are not the game host.')
+      }
+    })
+
+    socket.on('player_message', async (message) => {
+      io.local.emit('room_message', player.trans(), ': ' + message)
+    })
+
+    socket.on('disconnect', async () => {
+      if (!global.gameStarted)
+        await handleDisconnectInRoom(player, io)
+    })
+
+    socket.on('leave_game', async () => {
+      await handleDisconnectInGame(player, io)
+    })
+
+    socket.on('force_start', async () => {
+      let playerIndex = await getPlayerIndex(player.id)
+      if (global.players[playerIndex].forceStart === true) {
+        global.players[playerIndex].forceStart = false
+        --global.forceStartNum
+      } else {
+        global.players[playerIndex].forceStart = true
+        ++global.forceStartNum
+      }
+      io.local.emit('players_changed', global.players.map(player => player.trans()))
+      io.local.emit('force_start_changed', global.forceStartNum)
+
+      if (global.forceStartNum >= forceStartOK[global.players.length]) {
+        await handleGame(io)
+      }
+    })
+  }
+})
---
+++ src/server/map.js
@@ -0,0 +1,353 @@
+const Block = require('./block');
+const Point = require('./point');
+
+const directions = [
+  new Point(-1, -1), new Point(0, -1), new Point(1, -1),
+  new Point(-1, 0), new Point(0, 0), new Point(1, 0),
+  new Point(-1, 1), new Point(0, 1), new Point(1, 1)
+];
+
+function calcDistance(a, b) {
+  return Math.abs(a.x - b.x) + Math.abs(a.y - b.y);
+}
+
+function getRandomInt(min, max) {
+  var minInt = Math.ceil(min);
+  var maxInt = Math.floor(max);
+  return Math.floor(Math.random() * (maxInt - minInt)) + minInt;
+}
+
+class GameMap {
+  constructor(width, height, mountain, city, swamp, kings) {
+    this.width = parseInt(kings.length * 4 + 10 * width);
+    this.height = parseInt(kings.length * 4 + 10 * height);
+    if (mountain + city === 0) {
+      this.mountain = this.city = 0
+    } else {
+      this.mountain = parseInt(this.width * this.height / 4 * mountain / (mountain + city))
+      this.city = parseInt(this.width * this.height / 4 * city / (mountain + city))
+      console.log("mountains", this.mountain, "cities", this.city)
+    }
+    this.swamp = parseInt((this.width * this.height - this.mountain - this.city) / 3 * swamp)
+    this.kings = kings;
+    this.map = Array.from(Array(this.width), () => Array(this.height).fill(null));
+    this.turn = 0
+  }
+
+  getFather(conn, curPoint) {
+    while (conn[curPoint] !== curPoint) curPoint = conn[curPoint]
+    return curPoint
+  }
+
+  isObstacle(block) { return block.type === 'Mountain' || block.type === 'City' }
+  isPlain(block) { return block.type === 'Plain' }
+
+  checkConnection(obstacleCount) {
+    let conn = new Array(this.width * this.height)
+    let size = new Array(this.width * this.height)
+    for (let i = 0; i < conn.length; i++) { conn[i] = i, size[i] = 1 }
+    for (let i = 0; i < this.width; i++) {
+      for (let j = 0; j < this.height; j++) {
+        if (!this.isObstacle(this.map[i][j])) {
+          let curPoint = i * this.height + j;
+          (new Array(new Point(-1, 0), new Point(0, -1))).forEach((dir) => {
+            let tx = i + dir.x, ty = j + dir.y;
+            if (this.withinMap({x: tx, y: ty}) && !this.isObstacle(this.map[tx][ty])) {
+              let lastPoint = tx * this.height + ty;
+              let curFather = this.getFather(conn, curPoint);
+              let lastFather = this.getFather(conn, lastPoint);
+              if (size[lastFather] > size[curFather]) {
+                conn[curFather] = lastFather;
+                size[lastFather] += size[curFather];
+              } else {
+                conn[lastFather] = curFather;
+                size[curFather] += size[lastFather];
+              }
+            }
+          })
+        }
+      }
+    }
+    for (let i = 0; i < this.width; i++) {
+      for (let j = 0; j < this.height; j++) {
+        if (size[this.getFather(conn, i * this.height + j)] >= this.width * this.height - obstacleCount) {
+          return true
+        }
+      }
+    }
+    return false
+  }
+
+  generate() {
+    console.log("Width:", this.width, "Height:", this.height)
+    for (let i = 0; i < this.width; i++) {
+      for (let j = 0; j < this.height; j++) {
+        this.map[i][j] = new Block(i, j, 'Plain')
+      }
+    }
+    // Generate the king
+    for (let i = 0; i < this.kings.length; ++i) {
+      let pos = null;
+      while (true) {
+        let x = getRandomInt(0, this.width);
+        let y = getRandomInt(0, this.height);
+        pos = new Point(x, y);
+        let block = this.getBlock(pos);
+        if (block.type !== "King") {
+          let flag = true
+          for (let j = 0; j < i; ++j)
+            if (calcDistance(this.kings[j].king, new Point(x, y)) <= 6) {
+              flag = false
+              break
+            }
+          if (flag) {
+            block.initKing(this.kings[i]);
+            this.kings[i].initKing(block)
+            break;
+          }
+        }
+      }
+    }
+    // Generate the mountain
+    for (let i = 1; i <= this.mountain; ++i) {
+      let generated = false
+      for (let count = 3, x, y; count; --count) {
+        while (true) {
+          x = getRandomInt(0, this.width);
+          y = getRandomInt(0, this.height);
+          if (this.isPlain(this.map[x][y])) break;
+        }
+        this.map[x][y].type = 'Mountain'
+        if (this.checkConnection(i)) {
+          generated = true;
+          break
+        } else {
+          this.map[x][y].type = 'Plain'
+        }
+      }
+      if (!generated) { this.mountain = i - 1; console.log("Mountain Interrupted", i); break }
+    }
+    // Generate the city
+    for (let i = 1; i <= this.city; ++i) {
+      let generated = false
+      for (let count = 3, x, y; count; --count) {
+        while (true) {
+          x = getRandomInt(0, this.width);
+          y = getRandomInt(0, this.height);
+          if (this.isPlain(this.map[x][y])) break;
+        }
+        this.map[x][y].type = 'City'
+        if (this.checkConnection(i + this.mountain)) {
+          generated = true;
+          this.map[x][y].unit = getRandomInt(35, 55)
+          break
+        } else {
+          this.map[x][y].type = 'Plain'
+        }
+      }
+      if (!generated) { this.city = i - 1; console.log("City Interrupted", i); break }
+    }
+    // Generate the swamp.
+    for (let i = 1, x, y; i <= this.swamp; ++i) {
+      while (true) {
+        x = getRandomInt(0, this.width);
+        y = getRandomInt(0, this.height);
+        if (this.isPlain(this.map[x][y])) break;
+      }
+      this.map[x][y].type = 'Swamp'
+    }
+    let kings = this.kings
+    return new Promise(function (resolve, reject) {
+      console.log('Map generated successfully')
+      resolve(kings)
+    })
+  }
+
+  getTotal(player) {
+    let total = 0, count = 0
+    for (let i = 0; i < this.width; ++i) {
+      for (let j = 0; j < this.height; ++j) {
+        if (this.map[i][j].player === player) {
+          total += this.map[i][j].unit
+          ++count
+        }
+      }
+    }
+    return { army: total, land: count}
+  }
+
+  // initMap(data) {
+  //   data = data || Array.from(Array(this.width), () => Array(this.height).fill("Plain"));
+  //   for (let i = 0; i < this.width; i++) {
+  //     for (let j = 0; j < this.height; j++) {
+  //       this.map[i][j] = new Block(i, j, data[i][j])
+  //     }
+  //   }
+  // }
+
+  getBlock(point) {
+    return this.map[point.x][point.y]
+  }
+
+  ownBlock(player, point) {
+    return player === this.getBlock(point).player
+  }
+
+  transferBlock(point, player) {
+    this.map[point.x][point.y].player = player
+    this.map[point.x][point.y].unit = Math.floor(this.map[point.x][point.y].unit / 2)
+  }
+
+  withinMap(point) {
+    return 0 <= point.x && point.x < this.width && 0 <= point.y && point.y < this.height;
+  }
+
+  updateTurn() {
+    this.turn++;
+  }
+
+  updateUnit() {
+    for (let i = 0; i < this.width; i++) {
+      for (let j = 0; j < this.height; j++) {
+        switch (this.map[i][j].type) {
+          case "Plain":
+            if (this.map[i][j].player && this.turn % 50 === 0)
+              ++this.map[i][j].unit;
+            break;
+          case "King":
+            if (this.turn % 2 === 0)
+              ++this.map[i][j].unit;
+            break;
+          case "City":
+            if (this.map[i][j].player && this.turn % 2 === 0)
+              ++this.map[i][j].unit;
+            break;
+          case "Swamp":
+            if (this.map[i][j].player && this.turn % 2 === 0)
+              --this.map[i][j].unit;
+            if (this.map[i][j].unit === 0)
+              this.map[i][j].player = null;
+            break;
+          default:
+            break;
+        }
+      }
+    }
+  }
+
+  command(player, type, focusData, dirData) {
+    var focus = new Point(focusData.x, focusData.y);
+    var dir = new Point(dirData.x, dirData.y);
+    var newFocus = focus.move(dir);
+    if (this.commandable(player, focus, newFocus)) {
+      switch (type) {
+        case "Click":
+          this.moveAllMovableUnit(player, focus, newFocus);
+          break;
+        case "DoubleClick":
+          this.moveHalfMovableUnit(player, focus, newFocus);
+          break;
+        default:
+          console.warn("Unexpected type of command", type);
+      }
+    }
+  }
+
+  commandable(player, focus, newFocus) {
+    var isOwner = this.ownBlock(player, focus);
+    var possibleMove = this.withinMap(focus) && this.withinMap(newFocus);
+    var notMountain = this.getBlock(newFocus).type !== "Mountain";
+    return isOwner && possibleMove && notMountain
+  }
+
+  moveAllMovableUnit(player, focus, newFocus) {
+    var unit = this.getBlock(focus).getMovableUnit();
+    this.moveUnit(player, unit, focus, newFocus);
+  }
+
+  moveHalfMovableUnit(player, focus, newFocus) {
+    var unit = this.getBlock(focus).getMovableUnit();
+    var halfUnit = Math.ceil(unit / 2);
+    this.moveUnit(player, halfUnit, focus, newFocus);
+  }
+
+  moveUnit(player, unit, focus, newFocus) {
+    this.getBlock(focus).leaveUnit(unit);
+    this.getBlock(newFocus).enterUnit(player, unit);
+  }
+
+  buildBarrier() {
+    // TODO
+  }
+
+  // getViewWhole() {
+  //   var viewWhole = Array.from(Array(this.width), () => Array(this.height).fill(null));
+  //   for (let i = 0; i < this.width; i++) {
+  //     for (let j = 0; j < this.height; j++) {
+  //       viewWhole[i][j] = this.getBlock(new Point(i, j)).getView();
+  //     }
+  //   }
+  //   return viewWhole;
+  // }
+
+  getViewPlayer(player) {
+    // Get the view of the player from the whole map
+    console.log("Player is", player.username);
+    var viewPlayer = Array.from(Array(this.width), () => Array(this.height).fill(null));
+
+    for (let i = 0; i < this.width; i++) {
+      for (let j = 0; j < this.height; j++) {
+        var point = new Point(i, j);
+        var block = this.getBlock(point);
+        if (block.type === 'Mountain' || block.type === 'City') {
+          viewPlayer[i][j] = { type: 'Obstacle', color: null, unit: null };
+        } else {
+          viewPlayer[i][j] = { type: 'Fog', color: null, unit: null };
+        }
+      }
+    }
+    for (let i = 0; i < this.width; i++) {
+      for (let j = 0; j < this.height; j++) {
+        var point = new Point(i, j);
+        if (this.ownBlock(player, point)) {
+          viewPlayer[point.x][point.y] = {
+            type: this.map[point.x][point.y].type,
+            color: player.color,
+            unit: this.map[point.x][point.y].unit
+          }
+          directions.forEach(dir => {
+            const newPoint = point.move(dir);
+            if (this.withinMap(newPoint)) {
+              viewPlayer[newPoint.x][newPoint.y] = {
+                type: this.map[newPoint.x][newPoint.y].type,
+                color: this.map[newPoint.x][newPoint.y].player ?
+                  this.map[newPoint.x][newPoint.y].player.color : null,
+                unit: this.map[newPoint.x][newPoint.y].unit
+              }
+            }
+          })
+        }
+      }
+    }
+    return new Promise(function (resolve, reject) {
+      console.log('View of player generated successfully')
+      resolve(viewPlayer)
+    })
+  }
+
+  // makeKing() {
+  //   // Very dangerous and have upper bound for join new player
+  //   while (true) {
+  //     var x = getRandomInt(0, this.width);
+  //     var y = getRandomInt(0, this.height);
+  //     var block = this.getBlock(new Point(x, y));
+  //     if (block.type !== "King") {
+  //       block.setUnit(1);
+  //       block.setType("King");
+  //       return block
+  //     }
+  //   }
+  // }
+}
+
+module.exports = GameMap;
